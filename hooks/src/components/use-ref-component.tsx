ac0a77d36cd94f8fe985df16e4aab752a6f518d0
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-10-15 14:58:05 +0330

    adding useRef as a ref to the input for manipulating the input to focus

--- hooks/src/components/use-ref-component.tsx
+++ hooks/src/components/use-ref-component.tsx
@@ -2,21 +2,19 @@ import { useRef, useState } from 'react';
 
 const UseRefComponent = () => {
   const [inputValue, setInputValue] = useState('');
-  const value = useRef('');
+  const inputRef = useRef(null);
   const handleClick = () => {
-    value.current = inputValue;
+    inputRef.current.focus();
   };
-
-  // console.log(inputValue);
-  console.log(value.current);
 
   return (
     <div>
       <input
         onChange={(e) => setInputValue(e.target.value)}
         value={inputValue}
+        ref={inputRef}
       />
-      <button onClick={handleClick}>save the input value in Ref</button>
+      <button onClick={handleClick}>focus the input</button>
     </div>
   );
 };
