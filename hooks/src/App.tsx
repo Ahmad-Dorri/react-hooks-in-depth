04682b2585fee40d8e7f02bd6f895a27af3f7737
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-10-15 10:28:50 +0330

    adding a url for fetching a number and use it in useEffect combine with useState for re-fetching the call

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,10 +1,13 @@
 import StateComponentForm from './components/state-component-form';
+import UseEffectComponent from './components/use-effect-component';
 
 function App() {
   return (
     <>
       <h1>Use State: </h1>
       <StateComponentForm />
+      <h1>Use Effect: </h1>
+      <UseEffectComponent />
     </>
   );
 }
---
+++ hooks/src/components/use-effect-component.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const UseEffectComponent = () => {
+  const [url, setUrl] = useState(
+    'https://www.random.org/integers/?num=1&min=1&max=100&col=5&base=10&format=plain&rnd=new'
+  );
+  const [version, setVersion] = useState(0);
+  const [number, setNumber] = useState(0);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setNumber(data));
+  }, [url, version]);
+  return (
+    <>
+      <button onClick={() => setVersion((prevVersion) => prevVersion + 1)}>
+        change the number
+      </button>
+      <div key={version}>Your number is : {number}</div>
+    </>
+  );
+};
+
+export default UseEffectComponent;
