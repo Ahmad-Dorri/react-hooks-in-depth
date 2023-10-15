40f188aaa8606516d16a8dcf6b6495bc4591ac2b
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-10-15 09:04:24 +0330

    adding a form and changing the inputs using useState in correct way by creating new object and update the value that should changes

---
+++ hooks/src/App.tsx
@@ -0,0 +1,12 @@
+import StateComponent from './components/state-component';
+
+function App() {
+  return (
+    <>
+      <h1>Use State: </h1>
+      <StateComponent />
+    </>
+  );
+}
+
+export default App;
