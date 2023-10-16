c82cbfbe26717707f7c87d994cbbe099db97d0ec
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-10-16 11:34:24 +0330

    adding functionality for the changing the background using input value that we get from user

--- hooks/src/components/use-reducer-component.tsx
+++ hooks/src/components/use-reducer-component.tsx
@@ -1,8 +1,9 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
 enum ChangeColorActionKind {
   CHANGETOBLUE = 'CHANGETOBLUE',
   CHANGETORED = 'CHANGETORED',
+  CHANGETOYOURCOLOR = 'CHANGETOYOURCOLOR',
 }
 
 type ChangeColorAction = {
@@ -21,16 +22,13 @@ function changeColorReducer(
   const { type, payload } = action;
   switch (type) {
     case ChangeColorActionKind.CHANGETOBLUE:
-      return {
-        ...state,
-        color: payload,
-      };
-
+    case ChangeColorActionKind.CHANGETOYOURCOLOR:
     case ChangeColorActionKind.CHANGETORED:
       return {
         ...state,
         color: payload,
       };
+
     default:
       return state;
   }
@@ -40,6 +38,15 @@ const UseReducerComponent = () => {
   const [state, dispatch] = useReducer(changeColorReducer, {
     color: '#ff2',
   });
+  const [inputValue, setInputValue] = useState('');
+
+  const onSubmit = () => {
+    dispatch({
+      type: ChangeColorActionKind.CHANGETOYOURCOLOR,
+      payload: inputValue,
+    });
+  };
+
   return (
     <div
       style={{
@@ -64,6 +71,13 @@ const UseReducerComponent = () => {
         }>
         change the background to red
       </button>
+      <input
+        className="block"
+        placeholder="write the color"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={onSubmit}>Submit the color</button>
     </div>
   );
 };
